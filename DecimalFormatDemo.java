658cc7cbd0b9d3b42ede07e894504b8911016d59
Joshua Dilley <jldilley@iu.edu>
2025-07-20 13:00:01 -0500

    Add files via upload
    An example code from Absolute Java 6th edition that shows how DecimalFormat can be called and used.

---
+++ DecimalFormatDemo.java
@@ -0,0 +1,51 @@
+/*Name: Joshua Dilley
+Date: 07/20/2025
+Source Code: DecimalFormatDemo.java
+Action: This is an example code from Absolute Jave 6th Edition that shows how to 
+utilize the class DecimalFormat pulled from the java.text library.
+*/
+package absolutejava;
+
+import java.text.DecimalFormat;
+
+public class DecimalFormatDemo 
+{
+    public static void main(String[] args) 
+    {
+        DecimalFormat Pattern00dot000 = new DecimalFormat("00.000");
+        DecimalFormat Pattern0dot00 = new DecimalFormat("0.00");
+        
+        double d = 12.3456789;
+        System.out.println("Pattern 00.000");
+        System.out.println(Pattern00dot000.format(d));
+        System.out.println("Pattern 0.00");
+        System.out.println(Pattern0dot00.format(d));
+        
+        double Money = 19.8;
+        System.out.println("Pattern 0.00");
+        System.out.println("$" + Pattern0dot00.format(Money));
+        
+        DecimalFormat Percent = new DecimalFormat("0.00%");
+        
+        System.out.println("Pattern 0.00%");
+        System.out.println(Percent.format(0.308));
+        
+        DecimalFormat eNotation1 = new DecimalFormat("#0.###E0"); //1 or 2 digits before point
+        DecimalFormat eNotation2 = new DecimalFormat("00.###E0"); // 2 digits before point
+        
+        System.out.println("Pattern #0.###E0");
+        System.out.println(eNotation1.format(123.456));
+        System.out.println("Pattern 00.###E0");
+        System.out.println(eNotation2.format(123.456));
+        
+        double SmallNumber = 0.0000123456;
+        System.out.println("Pattern #0.###E0");
+        System.out.println(eNotation1.format(SmallNumber));
+        System.out.println("Pattern 00.###E0");
+        System.out.println(eNotation2.format(SmallNumber));
+        
+    }    
+}
+/* *************************Program Output************************************
+
+*/
